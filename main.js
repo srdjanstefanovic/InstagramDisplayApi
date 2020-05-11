e023be7aebf981f2974ed0b02a8e2cdb0b86bcff
srdjanstefanovic <srdjanstefanovicgit@gmail.com>
2020-05-11 22:28:48 +0200

    Probna verzija

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-$.getJSON("https://graph.instagram.com/17841431608152940?fields=id,username&access_token=IGQVJVX1VMdk1wRkJjdnpUcXRQYXRDN2FVWU10aE9veDE1dnBEbmZAZAbkEtZAjE3N3NpMFpvQ3loTFdlNHBIN0xib0k5Vk11MWFuSE1yRnROS2J1MWxzYVhueWJvUHlDaDZAOdHljU2o4Nk5LcEdCRHVKWkhpcnBEYWxBR2Vj", 
+const userId="";
+const token = "IGQVJVVjZANOGpCRFdQeVNndDNRaEpyajZAmekJHNHVvd0JwZAlRqRU03ZADJFcUZAFZAWtCdEJadWFMVXJPbEJmUDlfYU83d2dhd2x3b08ySndZANTEtM3gwcHlLLTNYU2h0aTc4NDZACaHZAZANl9Xci1PVkpRcFRJSUNJeGY4OF9v";
+
+$.getJSON("https://graph.instagram.com/17841431608152940?fields=id,username,media_count,account_type&access_token="+token, 
         function(data){
             
             $("#head-profil").text("Profil korisnika: "+data.username);
-            $("#profil-slika").attr("src",data.data.profile_picture);
-            $("#full-name").html("Puno ime korisnika > <strong>"+data.data.full_name+"</strong>");
-            $("#user-bio").html("Biografija korisnika > <i>"+data.data.bio+"</i>");
-            $("#user-website").text("Vebsajt korisnika > "+data.data.website);
-            $("#user-media").text("Broj objava > "+data.data.counts.media);
-            $("#user-followed-by").text("Broj pratilaca > "+data.data.counts.followed_by);
-            $("#user-follows").text("Prati > "+data.data.counts.follows);
+            $("#korisnicko-ime").text("Korisnicko ime: "+data.username);
+            $("#broj-objava").text("Broj objava: "+data.media_count);
+            $("#id-korisnika").text("Id korisnika: "+data.id);
+            $("#tip-naloga").text("Tip naloga: "+data.account_type);
+        });
 
-        })
+$.getJSON("https://graph.instagram.com/17841431608152940/media?fields=id,caption&access_token="+token, 
+        function(data){
+            var mediaId = data.data[0].id;
+            //alert(mediaId);
+            $("#opis-objave").text("Opis objave: "+data.data[0].caption);
+           
+
+        });
+
+        $.getJSON("https://graph.instagram.com/17851307273028969?fields=id,media_type,media_url,username,timestamp&access_token="+token, 
+        function(data){
+            
+            
+            $("#poslednja-slika").attr("src",data.media_url);
+            
+
+        });
+
+        
